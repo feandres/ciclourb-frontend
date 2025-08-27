f4771612d08ae095275b404c6bb971f1551ae926
Felipe Andrès <felipevsconcelos@gmail.com>
2025-08-27 19:52:55 -0300

    adiciona botão flutuante para voltar ao topo da página

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import ScrollToTopButton from "@/components/back-to-top-button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
       >
         <Navbar />
         <main className="flex-1">{children}</main>
+        <ScrollToTopButton />
         <Footer />
       </body>
     </html>
